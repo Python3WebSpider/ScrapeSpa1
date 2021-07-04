6e3a623d337483bcfef5669f0c8f7770d71d961c
Germey <cqc@cuiqingcai.com>
2021-07-05 00:56:37 +0800

    update url

--- spider2.py
+++ spider2.py
@@ -1,19 +1,20 @@
+import pymongo
 import requests
 import logging
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(levelname)s: %(message)s')
 
-INDEX_URL = 'https://dynamic1.scrape.center/api/movie/?limit={limit}&offset={offset}'
+INDEX_URL = 'https://spa1.scrape.center/api/movie/?limit={limit}&offset={offset}'
 
 MONGO_CONNECTION_STRING = 'mongodb://localhost:27017'
 MONGO_DB_NAME = 'movies'
 MONGO_COLLECTION_NAME = 'movies'
 
-import pymongo
 client = pymongo.MongoClient(MONGO_CONNECTION_STRING)
 db = client['movies']
 collection = db['movies']
+
 
 def scrape_api(url):
     logging.info('scraping %s...', url)
@@ -21,23 +22,30 @@ def scrape_api(url):
         response = requests.get(url)
         if response.status_code == 200:
             return response.json()
-        logging.error('get invalid status code %s while scraping %s', response.status_code, url)
+        logging.error('get invalid status code %s while scraping %s',
+                      response.status_code, url)
     except requests.RequestException:
         logging.error('error occurred while scraping %s', url, exc_info=True)
-        
+
+
 LIMIT = 10
+
 
 def scrape_index(page):
     url = INDEX_URL.format(limit=LIMIT, offset=LIMIT * (page - 1))
     return scrape_api(url)
 
+
 DETAIL_URL = 'https://dynamic1.scrape.center/api/movie/{id}'
+
 
 def scrape_detail(id):
     url = DETAIL_URL.format(id=id)
     return scrape_api(url)
 
+
 TOTAL_PAGE = 10
+
 
 def save_data(data):
     collection.update_one({
@@ -45,6 +53,7 @@ def save_data(data):
     }, {
         '$set': data
     }, upsert=True)
+
 
 def main():
     for page in range(1, TOTAL_PAGE + 1):
@@ -55,6 +64,7 @@ def main():
             logging.info('detail data %s', detail_data)
             save_data(detail_data)
             logging.info('data saved successfully')
+
 
 if __name__ == '__main__':
     main()
